0147d39587802d0d0843fe93a4b2653e5d7ffabd
Daniel <daniel.c.vicent@gmail.com>
2022-06-09 16:38:51 +0200

    Blog de daniel.

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
-TIME_ZONE = 'utc'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
